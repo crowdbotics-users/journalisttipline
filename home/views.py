3f66ff5cb2a612f57f31ca20aa0cfa6b275b15d6
Saurav <sp.gharti@gmail.com>
2018-06-13 17:40:22 +0000

    add package custom-user-roles

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'custom-user-roles', 'url': 'http://pypi.python.org/pypi/custom-user-roles/1.1.0'},
 	{'name':'datapunt-authorization-django', 'url': 'http://pypi.python.org/pypi/datapunt-authorization-django/0.2.18'},
     ]
     context = {
