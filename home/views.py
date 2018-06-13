14ac9d9819eb790f59c4a65c59ea7f843ec45d34
Saurav <sp.gharti@gmail.com>
2018-06-13 17:41:17 +0000

    add package django-profiling-dashboard

--- home/views.py
+++ home/views.py
@@ -7,6 +7,7 @@ from home.models import CustomText, HomePage
 
 def home(request):
     packages = [
+	{'name':'django-profiling-dashboard', 'url': 'http://pypi.python.org/pypi/django-profiling-dashboard/0.2'},
 	{'name':'custom-user-roles', 'url': 'http://pypi.python.org/pypi/custom-user-roles/1.1.0'},
 	{'name':'datapunt-authorization-django', 'url': 'http://pypi.python.org/pypi/datapunt-authorization-django/0.2.18'},
     ]
